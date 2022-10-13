c4c82293ec777f2364d2c2c0cc76fb5115aa218c
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-10-13 09:41:42 -0700

    Solved this Kata

--- theCouponCode-7kyu.js
+++ theCouponCode-7kyu.js
@@ -12,47 +12,84 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     let month = ["January", "February", "March", "April","May", "June","July","August","September","October","November","December"]
-    let currentDateArr = [...currentDate.replace(',', '').split(" ")];
-    let expirationDateArr = [...expirationDate.replace(',', '').split(" ")];
-  
-    let currentDateMonth = month.indexOf(currentDateArr[0]) +1;
-    let expirationDateMonth = month.indexOf(expirationDateArr[0])+1;
-  
+       
     
-   currentDateArr.splice(0,1,currentDateMonth);
+    
+    let currArr = currentDate.replace(',', '').split(" ");
+    let exArr = expirationDate.replace(',', '').split(" ");
+    
+     let currentMonth = month.indexOf(currArr[0])+1;
+    let expirationMonth = month.indexOf(exArr[0])+1;
   
-   expirationDateArr.splice(0,1,expirationDateMonth); 
+    currArr.splice(0,1,currentMonth)
+    exArr.splice(0,1,expirationMonth) 
+  
+    let curDate = currArr.map(x=>+x);
+    let exDate = exArr.map(x=>+x);
+    
+    console.log(curDate, exDate);
   
     
       if(enteredCode !== correctCode){
       return false
       }else{
+        if(curDate[2]<exDate[2])return true
+        else if(curDate[2]>exDate[2])return false
+        else if(curDate[0]<exDate[0])return true;
+        else if(curDate[0]>exDate[0])return false;
+        else if(curDate[1]<exDate[1])return true;
+        else if(curDate[1]>exDate[1]) return false;
+        else return true;
         
-              if(+currentDateArr[2] < +expirationDateArr[2]) return true
-              else if(+currentDateArr[0] < +expirationDateArr[0])
-              return true
-              else if(+currentDateArr[1]>+expirationDateArr[1])
-              return false
-          
-           return true
-      }
-        
+    }
   
        
           
   }
+    
+
+
+
+
+
+
+
+
+
+
+
+// -- first attempt 
+
+// function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+//     let month = ["January", "February", "March", "April","May", "June","July","August","September","October","November","December"]
+//     let currentDateArr = [...currentDate.replace(',', '').split(" ")];
+//     let expirationDateArr = [...expirationDate.replace(',', '').split(" ")];
+  
+//     let currentDateMonth = month.indexOf(currentDateArr[0]) +1;
+//     let expirationDateMonth = month.indexOf(expirationDateArr[0])+1;
+  
+    
+//    currentDateArr.splice(0,1,currentDateMonth);
+  
+//    expirationDateArr.splice(0,1,expirationDateMonth); 
+  
+    
+//       if(enteredCode !== correctCode){
+//       return false
+//       }else{
+        
+//               if(+currentDateArr[2] < +expirationDateArr[2]) return true
+//               else if(+currentDateArr[0] < +expirationDateArr[0])
+//               return true
+//               else if(+currentDateArr[1]>+expirationDateArr[1])
+//               return false
+          
+//            return true
+//       }
+        
+  
+       
+          
+//   }
