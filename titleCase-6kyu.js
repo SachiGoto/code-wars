f42954b64c6a31c401bf242ba20e6abe64cb930d
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-10-18 18:41:05 -0700

    Uploaded title case Kata

---
+++ titleCase-6kyu.js
@@ -0,0 +1,43 @@
+
+// Output
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+// ###Arguments (Haskell)
+
+// First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+// Second argument: the original string to be converted.
+// ###Arguments (Other languages)
+
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+// ###Example
+
+// titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+// titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+// titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+
+
+
+
+
+
+function titleCase(title, minorWords) {
+    // when there is no minorWords
+    if(minorWords == undefined){
+        let result = title.toLowerCase().split(" ").map(x=> x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
+        return result.join(" ");
+      }
+      
+    // When there are minorWords
+      let titleArr = title.toLowerCase().split(" "), minorWordsArr = minorWords.toLowerCase().split(" ");
+      let result = [];
+      
+      for(let i=0; i<titleArr.length; i++){
+        let str = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1).toLowerCase();
+        i === 0 || !minorWordsArr.includes(titleArr[i])?result.push(str):result.push(titleArr[i].toLowerCase());}
+      
+       return result.join(" ");
+      
+    }
