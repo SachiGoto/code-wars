100884c5a709275af3a5e08f5db3a3b72c8a4e1a
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-10-21 00:06:53 -0700

    Uploaded filter the number Kata

--- highestScoringWord-6kyu.js
+++ highestScoringWord-6kyu.js
@@ -39,3 +39,10 @@ function high(x){
     return x[index]
 
 }
+
+// great answer I found
+
+// function high(s){
+//     let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+//     return s.split(' ')[as.indexOf(Math.max(...as))];
+//   }
