62dbafcb16ceabf4fe3966449b9da2aced6b3270
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-12-08 23:02:11 -0800

    Working on longest common subsequence kata

---
+++ 5kyu/longest-common-subsequence-5kyu.js
@@ -0,0 +1,14 @@
+// Write a function called LCS that accepts two sequences and returns the longest subsequence common to the passed in sequences.
+
+// Subsequence
+// A subsequence is different from a substring. The terms of a subsequence need not be consecutive terms of the original sequence.
+
+// Example subsequence
+// Subsequences of "abc" = "a", "b", "c", "ab", "ac", "bc" and "abc".
+
+// LCS examples
+// LCS( "abcdef" , "abc" ) => returns "abc"
+// LCS( "abcdef" , "acf" ) => returns "acf"
+// LCS( "132535365" , "123456789" ) => returns "12356"
+
+
