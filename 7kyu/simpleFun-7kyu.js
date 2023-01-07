7854eec21f9acf10c1f32d75823376a2653777fc
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-01-06 22:40:21 -0800

    uploaded simple fun Kata

---
+++ 7kyu/simpleFun-7kyu.js
@@ -0,0 +1,24 @@
+
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+
+
+function reverseLetter(str) {
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    const result = [...str].reverse().map((letter)=> {return alphabet.includes(letter)?letter:''})
+    return result.join("")
+  
+  }
