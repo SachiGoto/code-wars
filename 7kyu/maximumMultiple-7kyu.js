a69dc3a6b690889a0426337b6d24b1bed39c6089
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-01-28 13:46:46 -0800

    uploaded maximum multiple kata

---
+++ 7kyu/maximumMultiple-7kyu.js
@@ -0,0 +1,24 @@
+
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+
+
+
+
+
+function maxMultiple(divisor, bound){
+
+    let arr = [], i = 0;
+    while(divisor*i <= bound){
+       arr.push(divisor*i)
+       i++}
+    
+    return arr[arr.length-1]
+  }
