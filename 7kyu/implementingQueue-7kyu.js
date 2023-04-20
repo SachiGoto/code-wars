da596229a6dc2a6d38a6afa17d47b63b91c25dd7
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-04-20 00:05:41 -0700

    worked on queue algorithm

--- 7kyu/implementingQueue-7kyu.js
+++ 7kyu/implementingQueue-7kyu.js
@@ -19,24 +19,98 @@
 
 
 
-var Queue = function() {
-    this.head = 0;
-    this.tail = 0;
-    this.storage = [];
-};
+// var Queue = function() {
+//     this.head = 0;
+//     this.tail = 0;
+//     this.storage = [];
+// };
 
-Queue.prototype.enqueue = function(item) {
-    this.storage[this.tail++] = item;
-};
+// Queue.prototype.enqueue = function(item) {
+//     this.storage[this.tail++] = item;
+// };
 
-Queue.prototype.dequeue = function() {
-    if (this.size()) {
-        var result = this.storage[this.head];
-        this.head++;
-        return result;
+// Queue.prototype.dequeue = function() {
+//     if (this.size()) {
+//         var result = this.storage[this.head];
+//         this.head++;
+//         return result;
+//     }
+// };
+
+// Queue.prototype.size = function() {
+//     return this.tail - this.head;
+// };
+
+// push and shift (add at the end, remove from the top)
+
+class Queue{
+    constructor(){
+        this.storage = {};
+        this.head = 0;
+        this.tail = 0;
     }
-};
+    
 
-Queue.prototype.size = function() {
-    return this.tail - this.head;
-};
+    enqueue(item){
+        this.storage[this.tail] = item;
+        this.tail++
+        
+     }
+
+     dequeue(){
+    let remove = this.storage[this.head]; 
+    delete this.storage[this.head];
+    this.head++ 
+    return remove
+     }
+}
+
+
+
+
+// let Queue = function() {
+//     this.storage = {}
+//     this.head = 0;
+//     this.tail = 0; 
+//     // implement your Queue constructor here
+//   };
+  
+  
+  
+//   Queue.prototype.enqueue = function(item) {
+//     this.storage[this.tail] = item
+//     this.tail++
+   
+//     // add item to the queue
+//   };
+  
+//   Queue.prototype.dequeue = function() {
+//     let remove = this.storage[this.head]; 
+//     delete this.storage[this.head];
+    
+//     this.head++ 
+//     return remove
+//     // remove item from the front of the queue and return its value
+//   };
+  
+//   Queue.prototype.size = function() {
+//     return Object.keys(this.storage).length
+  
+//     // return number of items in queue so far
+//   };
+
+
+;
+
+const queue = new Queue();
+
+queue.enqueue('hello')
+queue.enqueue('nice')
+queue.enqueue('sachimo')
+queue.dequeue()
+queue.dequeue()
+  console.log( queue )
+
+
+
+
